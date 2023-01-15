7049769953096458e5ed1e40b5bd1165d0bdf3f0
Nima <nimaneon17@gmail.com>
2023-01-15 14:59:32 +0330

    userList

--- dash/src/Pages/Users/UserList.js
+++ dash/src/Pages/Users/UserList.js
@@ -3,26 +3,51 @@ import { useState } from "react";
 import { usersRows } from "../../datas";
 import { DataGrid } from "@mui/x-data-grid";
 import { Link } from "react-router-dom";
-import './UserList.css'
+import Avatar from "@mui/material/Avatar";
+import "./UserList.css";
+
+
 export default function UserList() {
   const [usersData, setUsersData] = useState(usersRows);
-
+  const Button = ({ type }) => {
+    return <button className={"userListStatus " + type}>{type}</button>;
+  };
   const columns = [
-    { field: "id", headerName: "ID", width: 200 },
+    { field: "id", headerName: "ID", width: 100 },
     {
       field: "username",
       headerName: "User",
       width: 200,
       renderCell: (params) => {
         return (
-          <Link to="/">
-            <div className="userListUser">{params.row.username}</div>
+          <Link to="/" className="link userListUser">
+            <Avatar>
+              {params.row.avatar ? (
+                <img
+                  className="userListAvatar"
+                  src={params.row.avatar}
+                  alt={params.row.username}
+                />
+              ) : (
+                <Avatar />
+              )}
+            </Avatar>
+            <h5 className="userListUsername">{params.row.username}</h5>
           </Link>
         );
       },
     },
+    { field: "realname", headerName: "RealName", width: 200 },
     { field: "title", headerName: "Title", width: 200 },
-    { field: "status", headerName: "Status", width: 200 },
+    { field: "email", headerName: "Email", width: 300 },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 100,
+      renderCell: (params) => {
+        return <Button type={params.row.status} />;
+      },
+    },
   ];
 
   return (
